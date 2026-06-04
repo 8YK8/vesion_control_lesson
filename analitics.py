2e2fba06bd83c883f453b08201f7b6654abc0262
8YK8 <60570946+8YK8@users.noreply.github.com>
2026-06-04 20:08:49 +0300

    analytics

--- analitics.py
+++ analitics.py
@@ -1,4 +1,11 @@
+#import libraries
 import pandas as pd
-import numpy as np
+#import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+#create data frame from dictionary
 df = pd.DataFrame({'Name':['Olga', 'Maks', 'Slava'], 'Age':[22, 34, 45]})
-df.head()
+#print df head
+print (df.head())
+sns.barplot(data=df, x='Name', y='Age')
+plt.show()
